8cc33413ec034cbeb52fa9e229399a7ff1ea3fe0
Filippo Floramo <filifloramo@gmail.com>
2022-10-15 22:24:17 +0200

    test for env variable

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './styles.scss'
 import  app  from '/src/js/app.js'
-
+import bar from './src/js/boh'
 
 app();
+bar();
---
+++ src/js/boh.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export default function bar() {
+
+   async function foo() {
+
+      const response = await axios.get('https://api.goclimate.com/v1/flight_footprint?segments[0][origin]=CTA&segments[0][destination]=FCO&segments[1][origin]=FCO&segments[1][destination]=CTA&cabin_class=economy&currencies[]=SEK&currencies[]=USD', {
+         auth: {
+            API_KEY: import.meta.env.API_KEY
+        },
+         mode: 'no-cors'
+      });
+
+      console.log(response)
+
+   }
+   foo()
+}
+
+
+
+
+
+
+
